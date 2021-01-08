a441acfe872c8755d15bfc41072fb1236e25b658
Pablo Lopez <lopezmartinpablo@gmail.com>
2021-01-08 17:47:03 +0100

    Script en python que limpia el dataSet

--- Application/src/preProcessingSexoEdadProvincia.py
+++ Application/src/preProcessingSexoEdadProvincia.py
@@ -8,7 +8,7 @@ from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from pyspark.sql import SparkSession
 
-conf = SparkConf().setMaster('local[8]').setAppName('P3_spark.py')
+conf = SparkConf().setMaster('local[*]').setAppName('P3_spark.py')
 sc = SparkContext(conf = conf)
 
 spark = SparkSession(sc)
@@ -19,14 +19,18 @@ df = spark.read.csv("../DataSets/INE/sexo_edad_provincia_anyo.csv",header=True,s
 df2 = df.withColumn('Total', translate('Total', '.', ''))
 #Casteo la comlumna de total
 df3 = df2.withColumn("Total",df2["Total"].cast(DoubleType()))
-#Me quedo solo con el anyo 2020
+#Casteo el periodo para quedarme solo con las filas que pertenezcan al periodo de 2020
 df4 = df3.withColumn('Periodo', split(df['Periodo'], ' ').getItem(4))
 df5 = df4.withColumn("Periodo",df4["Periodo"].cast(DoubleType()))
 
-#Convierto las provincias a comunidades
-df6 = df5.filter(df5['Periodo'] > 2019).show()
+anyo = 20.0;
+df6 = df5.filter(df5['Periodo'] > anyo).show()
 
-#df6.coalesce(1).write.option("header", "true").option("sep", "\t").csv("sample_file.csv")
+
+#Convierto las provincias a comunidades
+#df6 = df5.filter(df5['Periodo'] > 2019).show()
+
+#df5.coalesce(1).write.option("header", "true").option("sep", ";").csv("sample_file.csv")
 
 #df2020.show() #Para mostrarlo por pantalla
 
